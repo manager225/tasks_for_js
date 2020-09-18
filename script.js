6c94904f158fb4f82571370ab7a18ca6281ced02
manager225 <manager225@mail.ru>
2020-09-18 22:55:05 +0300

    add task 2 and 3

--- script.js
+++ script.js
@@ -31,3 +31,77 @@ function normalize(num) {
     return str
 }
 console.log(getLuckyTickets())
+
+
+//**************************************************************
+
+//Задача 2
+//Дружественные числа на JavaScript
+/*Примером таких чисел может служить пара 220 и 284. Собственными делителями числа 220 являются следующие числа: 1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110. Сумма этих чисел 284. Собственными делителями числа 284, являются числа 1, 2, 4, 71, 142 и сумма этих чисел равна 220.*/
+/*Функция getFreindly, находит пары дружественных чисел в заданном промежутке от 1 до 9000 и возвращает их в виде двухмерного массива вида [ [220, 284], [1184, 1210], [2620, 2924] ].*/
+
+function getOwnDivisors(num) {
+    let arr = [];
+    for (let i = 1; i < num; i++) {
+        if (num % i == 0) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+
+function getSum(num) {
+    let sum = 0;
+    for (let elem of num) {
+        sum += elem;
+    }
+    return sum;
+}
+
+function getFreindly() {
+    let arr = [];
+    let min = 1;
+    let max = 9000;
+    for (; min < max; min++) {
+        let sum = getSum(getOwnDivisors(min));
+        let compare = getSum(getOwnDivisors(sum));
+        if (min == compare && min != sum) {
+            let tempArr = [];
+            tempArr.push(min, sum);
+            arr.push(tempArr);
+        }
+    }
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i][0] == arr[i + 1].reverse()[0]) {
+            arr.splice(i, 1);
+        }
+    }
+    return arr;
+    }
+console.log(getFreindly());
+
+
+//Задача 3
+//Совершенное число на JavaScript
+/*Совершенное число - целое число, равное сумме всех своих собственных делителей (то есть всех положительных делителей, отличных от самого числа). Функция getPerfect, находит совершенные числа в диапазоне от 1 до 1000.*/
+
+function getSum(num) {
+    let sum = 0;
+    for(let i = 1; i < num; i++){
+        if(num % i == 0){
+            sum += i;
+        }
+    }
+    return sum;
+}
+let res = []
+function getPerfect(num1,num2){
+    for(let i = num1; i <= num2; i++){
+        if(i == getSum(i)){
+            res.push(i)
+        }
+    }
+    return res
+}
+getPerfect(1,1000)
+console.log(res)
